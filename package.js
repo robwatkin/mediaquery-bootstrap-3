e2c1f072a6bb522c1bb842ca52dd5597752a2199
Rob Watkin <robwatkin@gmail.com>
2015-06-06 21:07:17 +0100

    Make package summary less than 100 chars

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
   name: 'robsw:mediaquery-bootstrap-3',
   version: '1.0.0',
   // Brief, one-line summary of the package.
-  summary: 'Sets (and exposes via template helpers) session variables for screen size, orientation and retina on bootstrap 3 brakepoints',
+  summary: 'Media query template helpers and session variables with bootstrap 3 brakepoints',
   // URL to the Git repository containing the source code for this package.
   git: 'https://github.com/robwatkin/mediaquery-bootstrap-3',
   // By default, Meteor will default to using README.md for documentation.
